e21d1822e2d6fa9daac960c226c765423c1723d0
Jaime <jaimeloeuf@gmail.com>
2021-04-10 18:37:08 +0800

    Move all into app and init firebase: 1.  Move all app code into app/ to add server into same repo later 2.  Init firebase for app repo for hosting

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="section">
     <div id="welcome" class="columns is-vcentered is-centered is-multiline">
+      <!-- Is the eager tag preventing the splash from loading? maybe not, since when logged in alr, it still does not load -->
+      <!-- <img
+      eager
+      alt="ClassExpress logo"
+      src="../assets/logo.png"
+      max-height="calc(100% - 1em)"
+      max-width="calc(100% - 1em)"
+      style="margin-top: calc(20%)"
+    /> -->
+
       <div class="column">
         <router-link
           :to="{ name: 'search' }"
@@ -24,6 +34,10 @@
           logout
         </button>
       </div>
+
+      <!-- <div>
+      <p>version: {{ buildTime + " " + commitHash.slice(0, 6).toString() }}</p>
+    </div> -->
     </div>
   </div>
 </template>
@@ -33,6 +47,13 @@ import logout from "../utils/logout";
 
 export default {
   name: "home",
+
+  data() {
+    return {
+      buildTime: process.env.buildTime,
+      commitHash: process.env.commitHash,
+    };
+  },
 
   methods: {
     logout,
