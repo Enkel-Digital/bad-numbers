24ed35c167ecc7a8a241b48df5a6732d41ae2c57
Jaime <jaimeloeuf@gmail.com>
2021-05-02 17:44:51 +0800

    [app] Add CSS loader component and use in Search: 1.  Add full screen CSS loader component to views. 2.  Update Search view to use the loader component before the search     result is loaded.

---
+++ app/src/views/Loader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="loader">
+    <b style="font-size: 1.4em">... LOADING ...</b>
+    <br />
+    <div id="spinner" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loader",
+};
+</script>
+
+<style scoped>
+#loader {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 100vh;
+}
+
+#spinner {
+  border: 0.3em solid #f3f3f3; /* Light grey */
+  border-top: 0.3em solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 3em;
+  height: 3em;
+  animation: spin 1.6s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
--- app/src/views/Search.vue
+++ app/src/views/Search.vue
@@ -2,8 +2,10 @@
   <div>
     <!-- Include the side nav bar component -->
 
+    <!-- Show full screen loader before search result is loaded -->
+    <loader v-if="searching" />
     <!-- Search results -->
-    <div class="cloumns">
+    <div v-else class="cloumns">
       <div class="column">Number: {{ num }}</div>
 
       <div v-if="search.reported">
@@ -21,16 +23,24 @@
       <div v-else>Great news, this number has not been reported before!</div>
     </div>
   </div>
+
+  <!-- a button to report this number! maybe not? -->
 </template>
 
 <script>
+import loader from "./Loader";
+
 export default {
   name: "search",
+
+  components: { loader },
 
   props: ["num"],
 
   data() {
     return {
+      searching: true,
+
       search: {
         // 0 because, this will be the number of times it has been reported, and 0 represents not reported before which will be the default
         // Actually change to something to else to show a loading screen? With CSS preferably
@@ -50,6 +60,9 @@ export default {
       reported: 2,
       reasons: ["Scam call", "Spam"],
     };
+
+    setTimeout(() => (this.searching = false), 1200);
+    // this.searching = false;
   },
 };
 </script>
