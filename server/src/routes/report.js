59e50d015575040f74c357a9bb6ff30c22a801ad
Jaime <jaimeloeuf@gmail.com>
2021-05-07 22:56:42 +0800

    [server] Refactor report API and DB schema: 1.  Update DB schema to store reported times and reports seperately. 2.  Refactor report API into 2 seperate DB functions to save data     following the new DB schema. 3.  Update .http test file to send data required following API update.

--- server/src/routes/report.js
+++ server/src/routes/report.js
@@ -10,8 +10,28 @@ const router = express.Router();
 const fs = require("../utils/fs");
 const FieldValue = require("firebase-admin").firestore.FieldValue;
 const incrementInstruction = FieldValue.increment(1);
-const deleteInstruction = FieldValue.delete();
 const { asyncWrap } = require("express-error-middlewares");
+
+// Report a number by updating the specific number's document, or create a new doc for it
+async function reportNumber(number) {
+  // Get ref to the Doc which is used both to check if it exists and to write data to it later on
+  const docRef = fs.collection("numbers").doc(number);
+
+  // READ the doc to check if it exists, EXTREMELY STUPID, but firebase cannot check for exists without a full read....
+  // See: https://www.reddit.com/r/Firebase/comments/fqxr3y/check_if_document_exists_without_reading_it/
+  const doc = await docRef.get();
+
+  // Check if this number has been reported before
+  // If doc no exists means first time being reported, so create the doc plus a reported value of 1
+  if (!doc.exists) await docRef.set({ reported: 1 });
+  else await docRef.update({ reported: incrementInstruction });
+}
+
+// Add a report/reason for why the number is reported in the reports collection
+async function addReason(number, reason) {
+  // Add a new document/"row" to the reports collection for the new report
+  return fs.collection("reports").add({ number, reason });
+}
 
 /**
  * Report a number and a reason for reporting it
@@ -22,28 +42,11 @@ router.post(
   "/",
   express.json(),
   asyncWrap(async (req, res) => {
-    // Read number from request body
-    const { number, reasonID } = req.body;
+    // Read number and reason for reporting from request body
+    const { number, reason } = req.body;
 
-    // Get ref to the Doc which is used both to check if it exists and to write data to it later on
-    const docRef = fs.collection("numbers").doc(number);
-
-    // READ the doc to check if it exists, EXTREMELY STUPID, but firebase cannot check for exists without a full read....
-    // See: https://www.reddit.com/r/Firebase/comments/fqxr3y/check_if_document_exists_without_reading_it/
-    const doc = await docRef.get();
-
-    // Check if this number has been reported before
-    // If doc no exists means first time being reported, so create the doc plus a count value of 1
-    if (!doc.exists)
-      await docRef.set({
-        count: 1,
-        // reason: { [reasonID]: incrementInstruction },
-      });
-    else
-      await docRef.update({
-        count: incrementInstruction,
-        // reason: { [reasonID]: incrementInstruction },
-      });
+    await reportNumber(number);
+    await addReason(number, reason);
 
     res.status(200).json({ ok: true });
   })
