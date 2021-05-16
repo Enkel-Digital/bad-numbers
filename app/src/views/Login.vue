cee73817b5a879d301a69888943599928b686bbd
Jaime <jaimeloeuf@gmail.com>
2021-05-16 19:00:19 +0800

    [app] WIP refactor login view.

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,31 +1,62 @@
 <template>
-  <div>
-    <h2>Login</h2>
+  <div class="columns center mx-4">
+    <h2 class="column mx-4 mt-4 pb-0">Login</h2>
 
-    Include ur country code please
-    <input
-      type="tel"
-      pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-      v-model="phoneNumber"
-      placeholder="Phone Number"
-      v-autofocus
-    />
-    <button id="sign-in-button" @click="sendOtp">Get OTP</button>
+    <div v-if="smsNotSent" class="column">
+      <label for="phoneNumber">
+        <b>Enter phone number with country code</b>
+      </label>
 
+      <!-- Pattern matches for a + sign and any number of digits -->
+      <input
+        type="tel"
+        pattern="[\+][0-9]+"
+        v-model="phoneNumber"
+        placeholder="Phone Number"
+        required
+        class="input mb-4"
+        style="width: 100%"
+        v-autofocus
+      />
+
+      <button class="button is-light is-fullwidth" @click="sendOtp">
+        Get OTP
+      </button>
+    </div>
+
+    <!-- Need to put width: 100% -->
+    <div v-else class="column" style="width: 100%">
+      <label for="otp">
+        <b>Enter OTP</b>
+      </label>
+
+      <!-- Pattern matches for a + sign and any number of digits -->
+      <input
+        type="number"
+        min="0"
+        inputmode="numeric"
+        pattern="[0-9]*"
+        v-model="otp"
+        placeholder="OTP"
+        required
+        class="input mb-4"
+        style="width: 100%"
+        v-autofocus
+      />
+
+      <div class="columns">
+        <div class="column is-half">
+          <button class="button is-light" @click="verifyOtp">Verify</button>
+        </div>
+
+        <div class="column is-half">
+          <button class="button is-light" @click="sendOtp">Resend OTP</button>
+        </div>
+      </div>
+    </div>
+
+    <!-- HTML element for g-recaptcha to insert their code -->
     <div id="recaptcha-container"></div>
-    <br />
-
-    <input
-      type="number"
-      min="0"
-      inputmode="numeric"
-      pattern="[0-9]*"
-      title="OTP"
-      v-model="otp"
-      placeholder="OTP"
-    />
-    <button @click="verifyOtp">Verify</button><br />
-    <button @click="sendOtp">Resend OTP</button>
   </div>
 </template>
 
@@ -36,10 +67,18 @@ import "firebase/auth";
 export default {
   data() {
     return {
+      // Used to toggle between showing phone number input and OTP input
+      smsNotSent: true,
+
       confirmationResult: undefined,
       phoneNumber: undefined,
       otp: undefined,
     };
+  },
+
+  mounted() {
+    // Initialise recaptcha once component is mounted
+    this.initReCaptcha();
   },
 
   methods: {
@@ -56,11 +95,14 @@ export default {
         // SMS sent. Prompt user to type the code from the message, then sign the
         // user in with confirmationResult.confirm(code).
         this.confirmationResult = confirmationResult;
+
+        // Show OTP input
+        this.smsNotSent = false;
+
         alert("SMS sent");
       } catch (error) {
-        // Error; SMS not sent
-        // ...
-        alert("Error ! SMS not sent");
+        // @todo Show error in console and alert box so easy for user to debug
+        alert("Error! SMS not sent");
       }
     },
 
@@ -76,16 +118,16 @@ export default {
         // Store this into vuex
         console.log("isNewUser: ", result.additionalUserInfo.isNewUser);
 
-        //route to set password !
         vm.$router.push({ name: "home" });
       } catch (error) {
         // User couldn't sign in (bad verification code?)
         console.log(error.message); // Show this on Screen
+
+        if (confirm("Re-Enter phone number?")) this.smsNotSent = true;
       }
     },
 
     initReCaptcha() {
-      // const vm = this;
       window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
         "recaptcha-container",
         {
@@ -99,9 +141,6 @@ export default {
         }
       );
     },
-  },
-  mounted() {
-    this.initReCaptcha();
   },
 };
 </script>
